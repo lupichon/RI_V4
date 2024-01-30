af272a8da26947cd6760c6aa4c3d549d2ac80de8
Lucas PICHON <lupichon@local.isima.fr>
2024-01-30 15:11:27 +0100

    laser couleur

--- Assets/Scripts/Alien.cs
+++ Assets/Scripts/Alien.cs
@@ -15,7 +15,7 @@ public class Alien : MonoBehaviour
     void Start()
     {
         _Joueur = GameObject.Find("XR Origin (XR Rig)");
-        _rifle = GameObject.Find("Box001");
+        _rifle = GameObject.Find("spawnlaser");
         _translationX = new Vector3(1, 0, 0);
         _translationZ = new Vector3(0, 0, 1);
         _rotBalles = new Quaternion(90, 0, 0,0);
@@ -71,9 +71,9 @@ public class Alien : MonoBehaviour
     void shot()
     {
         Vector3 pos = _rifle.transform.position;
-        pos.y = pos.y + 1.5f;
-        pos.z = pos.z + 0.9f;
-        pos.x = pos.x + 0.1f;
+        //pos.y = pos.y + 1.5f;
+        //pos.z = pos.z + 0.9f;
+        //pos.x = pos.x + 0.1f;
         if (_distance <= 10 && _animAlien.GetCurrentAnimatorStateInfo(0).IsName("shot"))
         {
        
@@ -81,21 +81,5 @@ public class Alien : MonoBehaviour
         }
     }
 }
-/*
-_spawnTimer += Time.deltaTime;
-if (_nbRock < 25 && _spawnTimer >= _spawnCooldown)
-{
-    _choixRock = Random.Range(0, 2);
-    switch (_choixRock)
-    {
-        case 0:
-            Instantiate(_prefabPlasmaWeakRock, _spawnPosition * Random.Range(50, 100) / 100, _rotationVide);
-            _spawnTimer = 0;
-            break;
-        case 1:
-            Instantiate(_prefabIonWeakRock, _spawnPosition * Random.Range(50, 100) / 100, _rotationVide);
-            _spawnTimer = 0;
-            break;
-    }
-}*/
+
 

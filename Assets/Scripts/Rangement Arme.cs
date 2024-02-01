5a803a7a8304063664c93ceace348d11555a1771
ValentinBoutouria <v.boutouria159@gmail.com>
2024-02-01 12:46:32 +0100

    ajout rangement auto des armes

---
+++ Assets/Scripts/Rangement Arme.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangementArme : MonoBehaviour
+{
+    public GameObject spawnpoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("SCI-FI"))
+        {
+            other.transform.position = spawnpoint.transform.position;
+            other.transform.rotation = spawnpoint.transform.rotation;
+           
+            Debug.Log("Test");
+
+
+        }
+    }
+}
